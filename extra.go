363b52b917ad7dd5e828df2b1535446016d82b2f
Fufu <fufuok@qq.com>
2026-01-20 16:31:41 +0800

    feat: add mp.MaxBlockingTasks

--- extra.go
+++ extra.go
@@ -29,23 +29,53 @@ func (p *poolCommon) TuneMaxBlockingTasks(size int) {
 	atomic.StoreInt32(&p.options.MaxBlockingTasks, int32(size))
 }
 
+// MaxBlockingTasks returns the maximum number of goroutines that are blocked each pool in multi-pool.
+func (mp *MultiPool) MaxBlockingTasks() int {
+	n := 0
+	for _, pool := range mp.pools {
+		n += pool.MaxBlockingTasks()
+	}
+	return n
+}
+
+// MaxBlockingTasks returns the maximum number of goroutines that are blocked each pool in multi-pool.
+func (mp *MultiPoolWithFunc) MaxBlockingTasks() int {
+	n := 0
+	for _, pool := range mp.pools {
+		n += pool.MaxBlockingTasks()
+	}
+	return n
+}
+
+// MaxBlockingTasks returns the maximum number of goroutines that are blocked each pool in multi-pool.
+func (mp *MultiPoolWithFuncGeneric[T]) MaxBlockingTasks() int {
+	n := 0
+	for _, pool := range mp.pools {
+		n += pool.MaxBlockingTasks()
+	}
+	return n
+}
+
 // TuneMaxBlockingTasks changes the maximum number of goroutines that are blocked each pool in multi-pool.
 func (mp *MultiPool) TuneMaxBlockingTasks(size int) {
+	perSize := size / len(mp.pools)
 	for _, pool := range mp.pools {
-		pool.TuneMaxBlockingTasks(size)
+		pool.TuneMaxBlockingTasks(perSize)
 	}
 }
 
 // TuneMaxBlockingTasks changes the maximum number of goroutines that are blocked each pool in multi-pool.
 func (mp *MultiPoolWithFunc) TuneMaxBlockingTasks(size int) {
+	perSize := size / len(mp.pools)
 	for _, pool := range mp.pools {
-		pool.TuneMaxBlockingTasks(size)
+		pool.TuneMaxBlockingTasks(perSize)
 	}
 }
 
 // TuneMaxBlockingTasks changes the maximum number of goroutines that are blocked each pool in multi-pool.
 func (mp *MultiPoolWithFuncGeneric[T]) TuneMaxBlockingTasks(size int) {
+	perSize := size / len(mp.pools)
 	for _, pool := range mp.pools {
-		pool.TuneMaxBlockingTasks(size)
+		pool.TuneMaxBlockingTasks(perSize)
 	}
 }
